7ad352b45faef92b61a7ea32f270b4b9c14a0581
Meskatul Islam <shakilmax58@gmail.com>
2024-03-29 01:23:06 +0600

    changed print message

--- sum.c
+++ sum.c
@@ -5,7 +5,7 @@ int main ()
     int a=5;
     int b=6;
     int sum =a+b;
-    printf("your calculation=%d",sum);
+    printf("shakil: your calculation=%d",sum);
 
     printf("\n your sub %d",a-b);
 }
